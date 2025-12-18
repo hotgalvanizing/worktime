7aa3c4ed14c21b09e88f555afb8f68de29f70bb9
liuhe <liuhe.mekp@bytedance.com>
2025-12-18 14:03:55 +0800

    更新端口9876

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ app.mount("/", StaticFiles(directory=".", html=True), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=9876)
